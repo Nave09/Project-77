62e0299b6e33451f159f03f78620022d9c231415
Nave09 <93429274+Nave09@users.noreply.github.com>
2022-10-01 10:12:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var word1 ="Quark"
+var word2 ="Strange"
+var a = 987654321
+var b = 123456789
+word2.concat(word1);
+console.log(word2);
+var add = a+b;
+console.log(add);
+var subtract = a-b;
+console.log(subtract);
+var multiply = a*b;
+console.log(multiply);
+var divide = a/b;
+console.log(divide);
+var sqrt_a = Math.sqrt(a);
+console.log(sqrt_a);
+var sqrt_b = Math.sqrt(b);
+console.log(sqrt_b);
